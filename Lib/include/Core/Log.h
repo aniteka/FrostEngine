e453580ea404d236073c9cae52c09b5a332aaeae
Nazariy <266444833552m@gmail.com>
2023-02-05 23:57:42 +0000

    feat: logger support

--- Lib/include/Core/Log.h
+++ Lib/include/Core/Log.h
@@ -1,20 +1,30 @@
 #pragma once
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
-namespace Core
+
+#define CREATE_LOGGER(loggerName) \
+	static auto loggerName = Core::Log::default_logger()->clone(#loggerName)
+
+namespace Core::Log
 {
-	namespace Log
+	using namespace spdlog;
+	namespace _defaultInit
 	{
-		using namespace spdlog;
-		namespace _defaultInit
+		class LogInit
 		{
-			struct LogInit
+		public:
+			LogInit()
 			{
-				LogInit()
-				{
-					
-				}
-			};
-		}
+				std::vector<sink_ptr> sinks;
+				sinks.push_back(std::make_shared<sinks::stdout_color_sink_mt>());
+				sinks.push_back(std::make_shared<sinks::basic_file_sink<std::mutex>>("logfile"));
+				auto logger = std::make_shared<Log::logger>("GlobalLogger", std::begin(sinks), std::end(sinks));
+				set_default_logger(logger);
+			}
+		};
+		static LogInit _init{};
 	}
 }
+
